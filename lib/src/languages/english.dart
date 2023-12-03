e14b5d07e495456583789b07707dd7538e8dad11
thamjeedpbr <thamjeedparamboor@gmail.com>
2023-12-03 09:23:40 +0530

    lang file

--- lib/src/languages/english.dart
+++ lib/src/languages/english.dart
@@ -57,7 +57,7 @@ const Map<String, String> enUS = {
   "paymentGateway": "Payment Gateway",
   "confirmation": "Confirmation",
   "successfulPayment": "Successful Payment",
-  "orderCompleted": "Order Conform",
+  "orderCompleted": "Order Confirmed",
   "orderNumber": "Order Number",
   "thankYouPurchasing": "Thank you for Purchasing",
   "continueShopping": "Continue Shopping",
